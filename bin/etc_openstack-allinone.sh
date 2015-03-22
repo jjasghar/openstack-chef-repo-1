eb7736198b3efc1e588ca9540de8d4913b3ec483
MIYATA Tadaaki <andes@flg.jp>
2015-03-22 17:42:14 +0900

    1.2

--- bin/etc_openstack-allinone.sh
+++ bin/etc_openstack-allinone.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -x
 sudo cp ~/configs/interfaces_openstack-allinone /etc/network/interfaces
 sudo cp ~/configs/hostname_openstack-allinone /etc/hostname
+sudo cp ~/configs/hosts_openstack-allinone /etc/hosts
 sudo cp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 sudo apt-get -y install openssh-server
 sudo reboot
