0637d824a6bd4a39d1d9f5da3ca6921d43c4bb31
andesm <andesm@nobu.flg.jp>
2015-03-28 14:42:03 +0900

    1.4

--- bin/etc_openstack-allinone.sh
+++ bin/etc_openstack-allinone.sh
@@ -3,5 +3,6 @@ sudo cp ~/configs/interfaces_openstack-allinone /etc/network/interfaces
 sudo cp ~/configs/hostname_openstack-allinone /etc/hostname
 sudo cp ~/configs/hosts_openstack-allinone /etc/hosts
 sudo cp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+sudo mkdir -p /etc/neutron/services/neutron-fwaas/
 sudo apt-get -y install openssh-server
 sudo reboot
--- bin/neutron.sh
+++ bin/neutron.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -x
+#!/bin/bash -x
 neutron net-create ext-net --router:external True --provider:physical_network external --provider:network_type flat
 neutron subnet-create ext-net --name ext-subnet --allocation-pool start=192.168.1.50,end=192.168.1.80 --disable-dhcp --gateway 192.168.1.13 192.168.1.0/24
 neutron router-create flg-router
