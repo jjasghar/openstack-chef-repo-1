e1ce83ed8d6f4871eaf666a3aa4542d13f42dcf2
MIYATA Tadaaki <andes@flg.jp>
2015-03-28 19:51:08 +0900

    1.4

--- bin/etc_openstack-allinone.sh
+++ bin/etc_openstack-allinone.sh
@@ -4,7 +4,6 @@ sudo apt-get -y dist-upgrade
 sudo apt-get -y autoremove
 sudo cp ~/configs/interfaces_openstack-allinone /etc/network/interfaces
 sudo cp ~/configs/hostname_openstack-allinone /etc/hostname
-sudo cp ~/configs/hosts_openstack-allinone /etc/hosts
 sudo cp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 sudo mkdir -p /etc/neutron/services/neutron-fwaas/
 ~/bin/chef-init_openstack-allinone.sh
