33cd1bd6feb371a7bd6ba10c719fd0ef6e749fee
MIYATA Tadaaki <andes@flg.jp>
2015-03-29 21:27:58 +0900

    1.4

--- bin/neutron.sh
+++ bin/neutron.sh
@@ -7,7 +7,7 @@ neutron subnet-create ext-net --name ext-subnet --allocation-pool start=192.168.
 neutron port-create ext-net --fixed-ip ip_address=192.168.1.40
 #
 neutron net-create flg-net
-neutron subnet-create flg-net --name flg-subnet --gateway 10.1.1.254 10.1.1.0/24
+neutron subnet-create flg-net --name flg-subnet --gateway 10.1.1.254 --dns-namservers 192.168.1.13 10.1.1.0/24
 #
 neutron router-create flg-router
 neutron router-gateway-set flg-router ext-net
