542d9ed2d003fbf37b8e7092a52bd0c3560e41d1
MIYATA Tadaaki <andes@flg.jp>
2015-03-29 16:47:53 +0900

    1.4

--- bin/deploy_chef-server.sh
+++ bin/deploy_chef-server.sh
@@ -42,11 +42,10 @@ cp .chef/encrypted_data_bag_secret ~/.chef/encrypted_data_bag_secret
 rm -rf .chef
 cd environments
 ln -s ~/configs/allinone.json .
-#cd ../roles
-#mv os-compute-single-controller.json os-compute-single-controller.json.bak
-#ln -s ~/configs/os-compute-single-controller.json .
-#mv os-compute-worker.json os-compute-worker.json.bak
-#ln -s ~/configs/os-compute-worker.json
+cd ../roles
+mv os-compute-single-controller.json os-compute-single-controller.json.bak
+ln -s ~/configs/os-compute-single-controller.json .
+ln -s ~/configs/os-object-storage.json
 cd ~/
 mkdir -p ~/.berkshelf/
 cp ~/configs/config.json ~/.berkshelf/
