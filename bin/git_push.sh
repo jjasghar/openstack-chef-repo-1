53cc6da5aad24773a20ba3145a910b7b0296ba48
MIYATA Tadaaki <andes@flg.jp>
2015-04-26 21:41:36 +0900

    1.7

--- bin/git_push.sh
+++ bin/git_push.sh
@@ -6,7 +6,7 @@ cd ~
 git config --global user.name "MIYATA Tadaaki"
 git config --global user.email andes@flg.jp
 git remote add origin https://github.com/andesm/openstack-chef-repo
-git add bin configs README.md
+git add -A bin configs README.md
 git commit -m $1
 git push -u origin master
 
