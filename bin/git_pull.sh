a07a854fe6df653569fe0e78af7416e8eeb98a50
MIYATA Tadaaki <andes@flg.jp>
2015-04-26 20:30:07 +0900

    1.7

---
+++ bin/git_pull.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+cd ~
+rm -rf .git
+git init
+git config --global user.name "MIYATA Tadaaki"
+git config --global user.email andes@flg.jp
+git remote add origin https://github.com/andesm/openstack-chef-repo
+git pull -u origin master
+
