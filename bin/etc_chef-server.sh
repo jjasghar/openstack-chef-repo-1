ec3b9fcfd8367220e0cbfc509cbc296762babe51
MIYATA Tadaaki <andes@flg.jp>
2015-05-04 22:34:59 +0900

    1.8

--- bin/etc_chef-server.sh
+++ bin/etc_chef-server.sh
@@ -2,7 +2,7 @@
 sudo cp ~/configs/interfaces_chef-server /etc/network/interfaces
 sudo cp ~/configs/hostname_chef-server /etc/hostname
 sudo cp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
-echo 'export LANG=C LC_ALL=C' >> ~/.bashrc 
+echo 'export LANG=C.UTF-8 LC_ALL=C.UTF-8' >> ~/.bashrc
 sudo apt-get -y update
 sudo apt-get -y install openssh-server
 sudo reboot
--- bin/etc_openstack-allinone.sh
+++ bin/etc_openstack-allinone.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -x
 sudo apt-get -y update
-sudo apt-get -y dist-upgrade
+#sudo apt-get -y dist-upgrade
 sudo apt-get -y autoremove
 sudo cp ~/configs/interfaces_openstack-allinone /etc/network/interfaces
 sudo cp ~/configs/hostname_openstack-allinone /etc/hostname
 sudo cp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+echo 'export LANG=C.UTF-8 LC_ALL=C.UTF-8' >> ~/.bashrc
 sudo mkdir -p /etc/neutron/services/neutron-fwaas/
 ~/bin/chef-init_openstack-allinone.sh
 sudo pvcreate /dev/sdb
 sudo vgcreate cinder-volumes /dev/sdb
-sudo apt-get -y install openssh-server openvswitch-switch mysql-client libmysqlclient-dev
+sudo apt-get -y install openssh-server openvswitch-switch
 sudo ovs-vsctl add-br br-pppoe
 sudo ovs-vsctl add-port br-pppoe eth2
 sudo reboot
