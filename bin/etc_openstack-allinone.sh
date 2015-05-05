a1a1f2ab638a178f01e1b370da8db7cc3134cd3b
MIYATA Tadaaki <andes@flg.jp>
2015-05-05 10:52:30 +0900

    1.8

--- bin/etc_openstack-allinone.sh
+++ bin/etc_openstack-allinone.sh
@@ -10,7 +10,8 @@ sudo mkdir -p /etc/neutron/services/neutron-fwaas/
 ~/bin/chef-init_openstack-allinone.sh
 sudo pvcreate /dev/sdb
 sudo vgcreate cinder-volumes /dev/sdb
-sudo apt-get -y install openssh-server openvswitch-switch mysql-client libmysqlclient-dev
+#sudo apt-get -y install openssh-server openvswitch-switch mysql-client libmysqlclient-dev
+sudo apt-get -y install openssh-server openvswitch-switch 
 sudo ovs-vsctl add-br br-pppoe
 sudo ovs-vsctl add-port br-pppoe eth2
 #sudo reboot
