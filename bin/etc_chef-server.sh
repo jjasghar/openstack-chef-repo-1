2216e91b366d819f4dc550df00e55e6ef84289f0
MIYATA Tadaaki <andes@flg.jp>
2015-05-05 14:10:34 +0900

    1.8

--- bin/etc_chef-server.sh
+++ bin/etc_chef-server.sh
@@ -2,7 +2,7 @@
 sudo cp ~/configs/interfaces_chef-server /etc/network/interfaces
 sudo cp ~/configs/hostname_chef-server /etc/hostname
 sudo cp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
-echo 'export LANG=C.UTF-8 LC_ALL=C.UTF-8' >> ~/.bash_profile
+echo 'export LANG=C.UTF-8 LC_ALL=C.UTF-8' >> ~/.profile
 sudo apt-get -y update
 sudo apt-get -y install openssh-server
 sudo reboot
