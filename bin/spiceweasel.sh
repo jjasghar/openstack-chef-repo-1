ae22fe3b090e863fdac2e496f9e9a0201a72bc4f
MIYATA Tadaaki <andes@flg.jp>
2015-05-10 13:28:11 +0900

    1.9

--- bin/spiceweasel.sh
+++ bin/spiceweasel.sh
@@ -2,4 +2,6 @@
 . ~/.bash_profile
 ~/bin/chef-init_chef-server.sh
 cd ~/git/openstack-chef-repo
-spiceweasel -e --novalidation --timeout 3600 infrastructure.yml
+spiceweasel --novalidation --timeout 3600 infrastructure.yml > spiceweasel.sh
+bash -x spiceweasel.sh
+
